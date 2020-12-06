458c7293570e8a96157ca4baba18ea3f57d296b3
Jun <kwh8857@naver.com>
2020-12-06 19:01:59 +0900

    fix(line)

--- src/components/Main/Bottom/Bottom.jsx
+++ src/components/Main/Bottom/Bottom.jsx
@@ -10,11 +10,39 @@ function Bottom() {
           return (
             <div className="one">
               <div className="greybar" />
-              <div className="box"></div>
+              <div className="box">
+                <div className="title">{item.title}</div>
+                <div className="content">{item.content}</div>
+              </div>
             </div>
           );
         } else {
-          return <div></div>;
+          return (
+            <div className="two">
+              <div className="greybar" />
+              <div className="box">
+                <div className="title">{item.title}</div>
+                <div className="content">
+                  {item.sub.map((item, idx) => {
+                    return (
+                      <div key={idx} className="sub">
+                        {item}
+                      </div>
+                    );
+                  })}
+                </div>
+                <div className="right">
+                  {item.content.map((item, idx) => {
+                    return (
+                      <div key={idx} className="right">
+                        {item}
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            </div>
+          );
         }
       })}
     </div>

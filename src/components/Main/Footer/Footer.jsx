59826a9d47d77e212015993c01d839378db8381b
GWAGJUN <kwh8857@naver.com>
2021-10-05 17:04:02 +0900

    fix

--- src/components/Main/Footer/Footer.jsx
+++ src/components/Main/Footer/Footer.jsx
@@ -7,14 +7,17 @@ function Footer() {
     <div className="footer">
       <img src="/assets/footer.svg" alt="푸터" />
       <div className="title">
-        {agnt === "MB"
-          ? `루미레이  |  사업자등록번호 123-12-12345 | 
-대표 장용진  |  경상북도 포항시 남구 지곡로 80,
- 5층 517호 (포항공과대학교 제1융합관)  | 
-samssonight@naver.com`
-          : `루미레이 | 사업자등록번호 123-12-12345 | 대표 장용진  경상북도
-        포항시 남구 지곡로 80, 5층 517호 (포항공과대학교 제1융합관) |
-        samssonight@naver.com`}
+        {agnt === "MB" ? (
+          `웹업  |  사업자등록번호 669-60-00332 | 대표 장용진
+              경상북도 포항시 남구 청암로 87 체인지업그라운드 6층 651호
+ 우편번호 37673  | samssonight@naver.com`
+        ) : (
+          <>
+            웹업 | 사업자등록번호 669-60-00332 | 대표 장용진 <br /> 경상북도
+            포항시 남구 청암로 87 체인지업그라운드 6층 651호 / 우편번호 37673 |
+            samssonight@naver.com
+          </>
+        )}
       </div>
     </div>
   );
